aa2fb9cebf312f5af36949a185987bb42081bcf2
Ruggiero Stello <rsstello@gmail.com>
2023-01-20 11:05:47 -0300

    Test Final refactor

--- src/main/java/FizzBuzz/FizzBuzz.java
+++ src/main/java/FizzBuzz/FizzBuzz.java
@@ -2,12 +2,14 @@ package FizzBuzz;
 
 public class FizzBuzz {
     public static String convert(int input) {
-        if (input % 15 == 0)
+        if (input % 15 == 0) {
             return "FizzBuzz";
-        if (input % 5 == 0)
+        }
+        else if (input % 5 == 0) {
             return "Buzz";
-        if (input % 3 == 0)
+        } else if (input % 3 == 0) {
             return "Fizz";
+        }
         return String.valueOf(input);
     }
 
