1c0e075a8469cf54685b7623dad0f0c37c216e04
Ruggiero Stello <rsstello@gmail.com>
2023-01-20 10:45:17 -0300

    Test Fizz green

--- src/main/java/FizzBuzz/FizzBuzz.java
+++ src/main/java/FizzBuzz/FizzBuzz.java
@@ -2,6 +2,8 @@ package FizzBuzz;
 
 public class FizzBuzz {
     public static String convert(int input) {
+        if (input == 3)
+            return "Fizz";
         return String.valueOf(input);
     }
 
